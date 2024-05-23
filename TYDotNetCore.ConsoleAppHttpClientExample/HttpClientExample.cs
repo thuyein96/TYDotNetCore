28dc8ce558d002d415f6ab8a39becf98389a6b72
thuyein96 <thuyein1996alpha@gmail.com>
2024-05-23 14:39:02 +0700

    [Complete] HttpClientExample

---
+++ TYDotNetCore.ConsoleAppHttpClientExample/HttpClientExample.cs
@@ -0,0 +1,114 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace TYDotNetCore.ConsoleAppHttpClientExample
+{
+    internal class HttpClientExample
+    {
+        private readonly HttpClient _client = new HttpClient() { BaseAddress = new Uri("https://localhost:7261") };
+        private readonly string _blogEndpoint = "api/blog";
+
+        public async Task AsyncRun()
+        {
+            //await AsyncRead();
+            //await AsyncEdit(3011);
+            //await AsyncCreate("test 1", "test 1", "test 1");
+            //await AsyncUpdate(3011, "Crazy Rich Asian", "Kevin Kwan", "Rich Asians");
+            await AsyncDelete(3011);
+        }
+
+        private async Task AsyncRead()
+        {
+            var response = await _client.GetAsync(_blogEndpoint);
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonStr = await response.Content.ReadAsStringAsync();
+                List<BlogDto> lst = JsonConvert.DeserializeObject<List<BlogDto>>(jsonStr)!;
+                foreach (var blog in lst)
+                {
+                    Console.WriteLine(blog.BlogTitle);
+                    Console.WriteLine(blog.BlogAuthor);
+                    Console.WriteLine(blog.BlogContent);
+                }
+            }
+        }
+
+        private async Task AsyncEdit(int id)
+        {
+            var response = await _client.GetAsync($"{_blogEndpoint}/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonStr = await response.Content.ReadAsStringAsync();
+                var item = JsonConvert.DeserializeObject<BlogDto>(jsonStr)!;
+                
+                Console.WriteLine(item.BlogTitle);
+                Console.WriteLine(item.BlogAuthor);
+                Console.WriteLine(item.BlogContent);
+            } else
+            {
+                string message = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(message);
+            }
+        }
+
+        private async Task AsyncCreate(string title, string author, string content)
+        {
+            BlogDto blog = new BlogDto()
+            {
+                BlogTitle = title,
+                BlogAuthor = author,
+                BlogContent = content
+            };
+
+            string blogJson = JsonConvert.SerializeObject(blog);
+
+            HttpContent httpContent = new StringContent(blogJson, Encoding.UTF8, Application.Json);
+            var response = await _client.PostAsync(_blogEndpoint, httpContent);
+            if(response.IsSuccessStatusCode)
+            {
+                string message = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(message);
+            }
+        }
+
+        private async Task AsyncUpdate(int id, string title, string author, string content)
+        {
+            BlogDto blog = new BlogDto()
+            {
+                BlogTitle = title,
+                BlogAuthor = author,
+                BlogContent = content
+            };
+
+            string blogJson = JsonConvert.SerializeObject(blog);
+
+            HttpContent httpContent = new StringContent(blogJson, Encoding.UTF8, Application.Json);
+            var response = await _client.PutAsync($"{_blogEndpoint}/{id}", httpContent);
+            if (response.IsSuccessStatusCode)
+            {
+                string message = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(message);
+            }
+        }
+
+        private async Task AsyncDelete(int id)
+        {
+            var response = await _client.DeleteAsync($"{_blogEndpoint}/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                string message = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(message);
+            }
+            else
+            {
+                string message = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
---
+++ TYDotNetCore.ConsoleAppHttpClientExample/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+using TYDotNetCore.ConsoleAppHttpClientExample;
+
+Console.WriteLine("Hello, World!");
+
+HttpClientExample httpClient = new HttpClientExample();
+await httpClient.AsyncRun();
+
+Console.ReadLine();
